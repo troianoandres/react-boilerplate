49f74f84078cb459a979ef9f4e068e542886550e
Ivan Diaz <ivan@opensupports.com>
2016-04-17 16:20:55 -0300

    [Ivan Diaz] - Add api/ajax utils

---
+++ application/lib/ajax-utils.js
@@ -0,0 +1,42 @@
+const _ = require('lodash');
+const $ = require('jquery');
+
+const AJAXUtils = {
+
+    getPromise(path, method, data) {
+        return (resolve, reject) => {
+            $.ajax({
+                url: path,
+                method: method,
+                data: data,
+                dataType: 'jsonp'
+            })
+                .done(resolve)
+                .fail((jqXHR, textStatus) => {
+                    reject(textStatus);
+                });
+        };
+    },
+
+    get(path) {
+        return new Promise(this.getPromise(path, 'GET'));
+    },
+
+    post(path, data) {
+        return new Promise(this.getPromise(path, 'POST', data));
+    },
+
+    patch(path, data) {
+        return new Promise(this.getPromise(path, 'PATCH', data));
+    },
+
+    put(path, data) {
+        return new Promise(this.getPromise(path, 'PUT', data));
+    },
+
+    del(path) {
+        return new Promise(this.getPromise(path, 'DELETE'));
+    }
+};
+
+module.exports = AJAXUtils;
---
+++ application/lib/api.js
@@ -0,0 +1,26 @@
+// VENDOR LIBS
+const _ = require('lodash');
+const path = require('path');
+const SessionStorage = require('sessionstorage');
+
+// APP LIBS
+const AJAXUtils = require('application/lib/ajax-utils');
+
+const rootURL = 'http://localhost:2020';
+
+function processData (data) {
+    return _.extend({
+        userId: SessionStorage.getItem('userId'),
+        token: SessionStorage.getItem('token')
+    }, data);
+}
+
+module.exports = {
+    call: function (path, method, data, callback) {
+        AJAXUtils[method](rootURL + path, processData(data)).then(callback);
+    },
+    setConfig: function (userId, token) {
+        SessionStorage.setItem('userId', userId);
+        SessionStorage.setItem('token', token);
+    }
+};
